68ce9904306175dc298c5a96b00e853dec58fa93
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-01-25 15:44:25 -0500

    changes

--- earlyAlertApi.php
+++ earlyAlertApi.php
@@ -9,7 +9,7 @@ if(empty($session_id) && !empty($_POST["session_id"])) session_id($_POST["sessio
 session_start();
 
 echo 'session_id: ' . session_id();
-echo var_dump($_SESSION['_basic_lti_context']);
+echo $_SESSION['_basic_lti_context']->oauth_consumer_key;
 
 if($_SESSION['_basic_lti_context']->oauth_consumer_key == $lti_auth['key']){
     $user_id = $_SESSION['_basic_lti_context']->user_id;
