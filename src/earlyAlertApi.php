414a747ecd79b4f87d9c5d4765ec877dbfb4ca06
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-01-25 15:02:27 -0500

    changes

--- src/earlyAlertApi.php
+++ src/earlyAlertApi.php
@@ -9,19 +9,21 @@ if(empty($a) && !empty($_POST["session_id"])) session_id($_POST["session_id"]);
 session_start();
 
 if($_SESSION['_basic_lti_context']->oauth_consumer_key == $lti_auth['key']){
-    $auditeeId = preg_match('/_(\d+)/', $_SESSION['_basic_lti_context']->user_id, $matches) ? $matches[1] : '-1';
-    $auditorId = $_POST["advisor"];
-    if (isset($_POST["advisor"])){
-        $response = addDeleteAuditor("POST",$auditorId, $auditeeId);
-        if ($response['Code']==200) {
-            echo 'Your advisor have access to all course progresses.';
-        }
-    } else{
-        $response = addDeleteAuditor("DELETE",$auditorId, $auditeeId);
-        if ($response['Code']==200) {
-            echo 'Your advisor no longer have access to all course progresses.';
-        }
-    }
+    // $user_id = $_SESSION['_basic_lti_context']->user_id;
+    // preg_match('/_(\d+)/', $user_id, $matches);
+    // $auditeeId = (bool) $matches ? $matches[1] : -1;
+    // $auditorId = $_POST["advisor"];
+    // if (isset($_POST["advisor"])){
+    //     $response = addDeleteAuditor("POST",$auditorId, $auditeeId);
+    //     if ($response['Code']==200) {
+    //         echo 'Your advisor have access to all course progresses.';
+    //     }
+    // } else{
+    //     $response = addDeleteAuditor("DELETE",$auditorId, $auditeeId);
+    //     if ($response['Code']==200) {
+    //         echo 'Your advisor no longer have access to all course progresses.';
+    //     }
+    // }
 } else {
     echo "Expired user session, please contact ".$supportEmail.' for support.';
 }
