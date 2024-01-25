fef8489f6000cc04d79a96c45ae651f50f67f80c
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-01-25 15:56:04 -0500

    changes

--- index.php
+++ index.php
@@ -26,6 +26,8 @@ if(isset($_REQUEST['lti_message_type'])) {    //Is this an LTI Request?
         $orgUnitId = $context->info['context_id'];
         $hasAuditor = hasAuditor($userId);
         $advisors = getAdvisors($orgUnitId);
+
+        if ($hasAuditor) echo 'hasAuditor'; else echo 'has no Auditor';
         
         include 'home.php';
     }
