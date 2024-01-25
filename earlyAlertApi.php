c507f3ca06ed48521af3c0ebf704ffc5fa0b3b4c
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-01-25 15:30:15 -0500

    chn

--- earlyAlertApi.php
+++ earlyAlertApi.php
@@ -3,12 +3,13 @@ require_once("src/functions.php");
 require_once("src/info.php");
 
 //Detects if session_id is empty, then looks to see if one has been stashed in a hidden form item to allow cross domain requests
-$a = session_id();
-if(empty($a) && !empty($_POST["session_id"])) session_id($_POST["session_id"]); 
+$session_id = session_id();
+if(empty($session_id) && !empty($_POST["session_id"])) session_id($_POST["session_id"]); 
 
 session_start();
 
-echo 'key: ' . $_SESSION['_basic_lti_context']->oauth_consumer_key;
+echo 'session_id: ' . $session_id;
+echo var_dump($_SESSION['_basic_lti_context']);
 
 if($_SESSION['_basic_lti_context']->oauth_consumer_key == $lti_auth['key']){
     $user_id = $_SESSION['_basic_lti_context']->user_id;
