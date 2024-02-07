26f46484115c290d58f34fb7fd9712b61c3b4061
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-02-07 16:01:30 -0500

    change

--- src/functions.php
+++ src/functions.php
@@ -46,9 +46,9 @@ function getMyAuditors($auditeeId){
     global $config;
     $result ='';
     $response = doValenceRequest('GET', '/d2l/api/le/'.$config['LE_Version'].'/auditing/auditees/'.$auditeeId); 
+    echo var_dump($response['response']->Auditors);
     if (!empty($response['response']->Auditors)){
         $myAuditors = array_column($response['response']->Auditors, 'AuditorId');
-        echo var_dump('Not empty: '. $myAuditors);
         $result = implode(',', $myAuditors);
     }
     return $result;
