2511e5bcd16ed2102ce044866dba807b6a49bc7a
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-02-22 09:54:18 -0500

    change

--- home.php
+++ home.php
@@ -39,16 +39,19 @@
         }
         $advisorSelect .= '</select></div>';
 
-        $termsSelect = '<div class="mb-3">
-        <select class="form-select" id="terms" required name="terms" >
-          <option value="" disabled selected>Select term</option>';
-        foreach($groups as $group){
-            $termsSelect .= '<option value="'.$group['groupId'].'">'.$group['Name'].'</option>';
+        // $termsSelect = '<div class="mb-3">
+        // <select class="form-select" id="terms" required name="terms" >
+        //   <option value="" disabled selected>Select term</option>';
+        // foreach($groups as $group){
+        //     $termsSelect .= '<option value="'.$group['groupId'].'">'.$group['Name'].'</option>';
+        // }
+        // $termsSelect .= '</select></div>';
+
+
+        if (!$hasAuditor) {
+          echo $advisorSelect; 
+          //echo $termsSelect;
         }
-        $termsSelect .= '</select></div>';
-
-
-        if (!$hasAuditor) {echo $advisorSelect; echo $termsSelect;}
       ?>
       <div class="mb-3">
         <button type="submit" class="btn btn-primary" id="early_alert_btn"><?php if ($hasAuditor) echo 'Cancel'; else echo 'Consent';?></button>
