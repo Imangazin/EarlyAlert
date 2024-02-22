f7268f6e185e67b66616381f33249dbaaed07236
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-02-22 09:54:57 -0500

    change

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if(isset($_REQUEST['lti_message_type'])) {    //Is this an LTI Request?
         $orgUnitId = $context->info['context_id'];
 
         $groupCategoryId = getGroupCategoryId($orgUnitId);
-        $groups = getGroups($orgUnitId, $groupCategoryId);
+        //$groups = getGroups($orgUnitId, $groupCategoryId);
 
         $hasAuditor = hasAuditor($userId);
         $advisors = getAdvisors($orgUnitId);
