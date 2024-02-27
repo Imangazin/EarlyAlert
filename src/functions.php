a147991d369e460146f69da466f252b9987a014f
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-02-27 10:22:55 -0500

    change

--- src/functions.php
+++ src/functions.php
@@ -110,8 +110,8 @@ function getGroups($orgUnitId, $categoryId){
     $bookmark = '';
     $data = array();
     $terms_response = doValenceRequest('GET', '/d2l/api/lp/'.$config['LP_Version'].'/orgstructure/?orgUnitType=5&orgUnitCode=20&bookmark='.$bookmark);
-    echo $terms_response['response']['PagingInfo']['HasMoreItems'];
-    echo $terms_response['response']['PagingInfo']['Bookmark'];
+    echo $terms_response['response']->PagingInfo->HasMoreItems;
+    echo $terms_response['response']->PagingInfo->Bookmark;
     // while ($hasMore){
     //     $terms_response = doValenceRequest('GET', '/d2l/api/lp/'.$config['LP_Version'].'/orgstructure/?orgUnitType=5&orgUnitCode=20&bookmark='.$bookmark);
     //     foreach($terms_response['response']['Items'] as $item){
