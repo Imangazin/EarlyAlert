670b8cf477bcab72a105f030d1730174f9e3748f
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-03-01 14:22:49 -0500

    chnage

--- index.php
+++ index.php
@@ -4,20 +4,24 @@ require_once("src/functions.php");
 // Load up the LTI Support code
 require_once 'ims-blti/blti.php';
 
+//setting user sessions
+$currentCookieParams = session_get_cookie_params();
+
+if (isChromeOrEdge()) echo "it is chrome <br>"; else echo 'it is not chrome <br>';
+
+if (isChromeOrEdge()) $cookie_loation = $cookie_loation . ' partitioned;';
+session_set_cookie_params(
+    $currentCookieParams["lifetime"],
+    $cookie_loation,
+    $_SERVER['HTTP_HOST'],
+    "1",
+    "1"
+);
+
+session_start();
+
 //All of the LTI Launch data gets passed through in $_REQUEST
 if(isset($_REQUEST['lti_message_type'])) {    //Is this an LTI Request?
-    //setting user sessions
-    $currentCookieParams = session_get_cookie_params();
-    if (isChromeOrEdge()) $cookie_loation = $cookie_loation . ' partitioned;';
-    session_set_cookie_params(
-        $currentCookieParams["lifetime"],
-        $cookie_loation,
-        $_SERVER['HTTP_HOST'],
-        "1",
-        "1"
-    );
-
-    session_start();
     $context = new BLTI($lti_auth['secret'], true, false);
 
     if($context->complete) exit(); //True if redirect was done by BLTI class
