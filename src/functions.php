89b6d9271045e25a08774988c3216bb256e7ce45
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-03-04 14:33:04 -0500

    test

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,12 @@
 require_once("info.php");
 require_once("doValence.php");
 
+function isChromeOrEdge() {
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    //echo  "User agent: ". $userAgent . "<br>";
+    return (strpos($userAgent,'Chrome') !== false || strpos($user, 'Edg') !== false);
+}
+
 function hasAuditor($userId){
     global $config;
     $response = doValenceRequest('GET', '/d2l/api/le/'.$config['LE_Version'].'/auditing/auditees/'.$userId);
