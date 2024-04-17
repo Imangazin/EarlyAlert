d5cd3fb7543ed264e223f8f2146b8ea5ba764e56
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-04-17 13:49:16 -0400

    test

--- src/functions.php
+++ src/functions.php
@@ -54,7 +54,8 @@ function addDeleteAuditor($verb, $auditorId, $auditeeId){
 
 //returns current academic turn
 function getCurrentAcademicTerm() {
-    $currentMonth = date('n');
+    //$currentMonth = date('n');
+    $currentMonth = 10;
     $currentYear = date('Y');
     if ($currentMonth >= 1 && $currentMonth <= 4) {
         return "Winter-".$currentYear;
